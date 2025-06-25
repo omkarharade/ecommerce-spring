1dc66f808e9e04d8ab44f347117f50ecac39b0df
Omkar Harade <omkarharade1301@gmail.com>
2025-06-25 18:47:41 +0530

    initial commit

---
+++ src/main/java/com/example/ecommerceSpring/EcommerceSpringApplication.java
@@ -0,0 +1,15 @@
+package com.example.ecommerceSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EcommerceSpringApplication.class, args);
+
+	}
+
+}
